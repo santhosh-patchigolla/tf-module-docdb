3f329d3ec5b1151f5d7741afe962b52b2f141c61
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-31 19:27:36 +0530

    sdc

--- sg.tf
+++ sg.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "allows_docdb" {
   name        = "roboshop allows_docdb_internal only"
   description = "allows_docdb_internal only"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
     description = "Docdb from VPC"
