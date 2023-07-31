917168020fad21fc3c05e9f88c74fd7294b49a0c
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-31 16:57:30 +0530

    sdc

--- docdb.tf
+++ docdb.tf
@@ -4,11 +4,30 @@ resource "aws_docdb_cluster" "docdb" {
   master_username          = "admin1"
   master_password          = "roboshop1"
   skip_final_snapshot      = true  
+  vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
+  db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name  
 }  
 
 
 
 
 
-/* //  vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
-  db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name */
+# Creating the subnet groupID from below
+resource "aws_docdb_subnet_group" "docdb_subnet_group" {
+  name       = "roboshop-docdb-${var.ENV}-subnetgroup"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-docdb-${var.ENV}-subnetgroup"
+  }
+}
+
+# Creates Compute Machines needed for DocumentDB and these has to be attached to the cluster
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "roboshop-${var.ENV}-docdb-instance"
+  cluster_identifier = aws_docdb_cluster.docdb.id                      #This argument attaches the nodes created here to the docdb cluster.
+  instance_class     = "db.t3.medium"
+
+  depends_on = [aws_docdb_cluster.docdb]
+}
